295980cbeb4131cf80977ae02c3994071ec443a4
igo4ek <wawka2002@gmail.com>
2022-10-22 18:20:56 +0000

    Сгенерированы сущности User, Order, Product, а также реализованы связи

---
+++ src/Entity/Customer.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CustomerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CustomerRepository::class)
+ */
+class Customer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Ord::class, mappedBy="customer")
+     */
+    private $ords;
+
+    public function __construct()
+    {
+        $this->ords = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Ord>
+     */
+    public function getOrds(): Collection
+    {
+        return $this->ords;
+    }
+
+    public function addOrd(Ord $ord): self
+    {
+        if (!$this->ords->contains($ord)) {
+            $this->ords[] = $ord;
+            $ord->setCustomer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrd(Ord $ord): self
+    {
+        if ($this->ords->removeElement($ord)) {
+            // set the owning side to null (unless already changed)
+            if ($ord->getCustomer() === $this) {
+                $ord->setCustomer(null);
+            }
+        }
+
+        return $this;
+    }
+}
