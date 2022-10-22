27d3f4516d8e284bc952795d1ea3faf3bb63c926
igo4ek <wawka2002@gmail.com>
2022-10-22 19:59:40 +0000

    Добавление фикстур продуктов

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,16 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class ProductFixtures extends Fixture
 {
+    private $fixtures = [
+        [
+            "name" => "Камень Ryzen 5 2600G",
+            "price" => 15999.99
+        ],
+        [
+            "name" => "Слойка со сгущёнкой",
+            "price" => 45
+        ],
+        [
+            "name" => "Виниловый проигрыватель",
+            "price" => 8500.67
+        ],
+        [
+            "name" => "Бумажный пакет для продуктов",
+            "price" => 6.78
+        ],
+    ];
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+         foreach ($this->fixtures as $fixture) {
+            $product = new Product();
+            $product->setName($fixture["name"]);
+            $product->setPrice($fixture["price"]);
+            $manager->persist($product);
+         }
+         $manager->flush();
     }
 }
