35bbd6ef4a970977c296add31e66932d3008a7af
igo4ek <wawka2002@gmail.com>
2022-10-23 09:12:24 +0000

    Фикстуры перенесены в AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,43 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private array $productFixtures = [
+        [
+            "id" => 1,
+            "name" => "Камень Ryzen 5 2600G",
+            "price" => 15999.99
+        ],
+        [
+            "id" => 2,
+            "name" => "Слойка со сгущёнкой",
+            "price" => 45
+        ],
+        [
+            "id" => 3,
+            "name" => "Виниловый проигрыватель",
+            "price" => 8500.67
+        ],
+        [
+            "id" => 4,
+            "name" => "Бумажный пакет для продуктов",
+            "price" => 6.78
+        ],
+    ];
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        foreach ($this->productFixtures as $productFixture) {
+            $product = new Product();
+            $product->setId($productFixture["id"]);
+            $product->setName($productFixture["name"]);
+            $product->setPrice($productFixture["price"]);
+            $manager->persist($product);
+        }
         $manager->flush();
     }
 }
