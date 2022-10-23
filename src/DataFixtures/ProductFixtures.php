1b66b4e639debdd29db80462e7233088644d0e77
igo4ek <wawka2002@gmail.com>
2022-10-23 12:37:15 +0000

    Revert "Добавление фикстур продуктов"
    This reverts commit 27d3f4516d8e284bc952795d1ea3faf3bb63c926.

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,38 +2,16 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class ProductFixtures extends Fixture
 {
-    private $fixtures = [
-        [
-            "name" => "Камень Ryzen 5 2600G",
-            "price" => 15999.99
-        ],
-        [
-            "name" => "Слойка со сгущёнкой",
-            "price" => 45
-        ],
-        [
-            "name" => "Виниловый проигрыватель",
-            "price" => 8500.67
-        ],
-        [
-            "name" => "Бумажный пакет для продуктов",
-            "price" => 6.78
-        ],
-    ];
     public function load(ObjectManager $manager): void
     {
-         foreach ($this->fixtures as $fixture) {
-            $product = new Product();
-            $product->setName($fixture["name"]);
-            $product->setPrice($fixture["price"]);
-            $manager->persist($product);
-         }
-         $manager->flush();
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
     }
 }
