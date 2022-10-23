49c496b35eef2085ca44639419bda922c9748ce2
igo4ek <wawka2002@gmail.com>
2022-10-23 12:37:49 +0000

    Revert "composer require --dev orm-fixtures"
    This reverts commit 8d2a4a39cb068d51dd8d1f2cfbfe462382d07865.

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
