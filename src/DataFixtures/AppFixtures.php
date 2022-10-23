742d8b3d3255f8dc42977922f238484848b462bd
igo4ek <wawka2002@gmail.com>
2022-10-23 12:36:53 +0000

    Revert "Фикстуры перенесены в AppFixtures"
    This reverts commit 35bbd6ef4a970977c296add31e66932d3008a7af.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,43 +2,16 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    private array $productFixtures = [
-        [
-            "id" => 1,
-            "name" => "Камень Ryzen 5 2600G",
-            "price" => 15999.99
-        ],
-        [
-            "id" => 2,
-            "name" => "Слойка со сгущёнкой",
-            "price" => 45
-        ],
-        [
-            "id" => 3,
-            "name" => "Виниловый проигрыватель",
-            "price" => 8500.67
-        ],
-        [
-            "id" => 4,
-            "name" => "Бумажный пакет для продуктов",
-            "price" => 6.78
-        ],
-    ];
     public function load(ObjectManager $manager): void
     {
-        foreach ($this->productFixtures as $productFixture) {
-            $product = new Product();
-            $product->setId($productFixture["id"]);
-            $product->setName($productFixture["name"]);
-            $product->setPrice($productFixture["price"]);
-            $manager->persist($product);
-        }
+        // $product = new Product();
+        // $manager->persist($product);
+
         $manager->flush();
     }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -32,13 +32,6 @@ class Product
         return $this->id;
     }
 
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
     public function getName(): ?string
     {
         return $this->name;
