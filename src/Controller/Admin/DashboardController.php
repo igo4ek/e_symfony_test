40306ba004d28fbcf443788b2f505ecbc0c4e5a3
igo4ek <wawka2002@gmail.com>
2022-10-23 21:04:10 +0000

    Revert "php bin/console make:admin:crud для Products"
    This reverts commit 9b8541026667acaaf95fb4bb46828926fa312e3b.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Products;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -28,6 +27,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Products', 'fas fa-list', Products::class);
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
